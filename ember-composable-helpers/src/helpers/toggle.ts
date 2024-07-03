f43f371f3ddaf630088ee50dec655174bc3ae0c9
Charles Fries <charliefries@icloud.com>
2024-07-03 11:15:22 -0700

    Remove generic contraint

--- ember-composable-helpers/src/helpers/toggle.ts
+++ ember-composable-helpers/src/helpers/toggle.ts
@@ -11,18 +11,18 @@ function nextIndex(length: number, currentIdx: number) {
   return currentIdx + 1;
 }
 
-export function toggle<T extends object>([prop, obj, ...values]: [keyof T, T, ...(T[keyof T])[]]) {
+export function toggle<T>([prop, obj, ...values]: [keyof T, T, ...(T[keyof T])[]]) {
   return function() {
-    let currentValue = get(obj, prop);
+    let currentValue = get(obj as object, prop as string);
 
     if (isPresent(values)) {
-      let currentIdx = values.indexOf(currentValue);
+      let currentIdx = values.indexOf(currentValue as T[keyof T]);
       let nextIdx = nextIndex(values.length, currentIdx);
 
-      return set(obj, prop as string, values[nextIdx]!);
+      return set(obj as object, prop as string, values[nextIdx]!);
     }
 
-    return set(obj, prop as string, !currentValue);
+    return set(obj as object, prop as string, !currentValue);
   };
 }
 
