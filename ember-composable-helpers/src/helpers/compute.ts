6d88f45b9cbf308df4356099b74574f5ac53e0b1
Peter Wagenet <peter@wagenet.us>
2025-01-06 08:30:29 -0800

    Fix compute types and add type tests

--- ember-composable-helpers/src/helpers/compute.ts
+++ ember-composable-helpers/src/helpers/compute.ts
@@ -1,6 +1,7 @@
 import { helper } from '@ember/component/helper';
+import type { AnyFn } from '../utils/types';
 
-export function compute([action, ...params]: [(...args: never[]) => unknown]) {
+export function compute<Action extends AnyFn> ([action, ...params]: [action: Action, ...params: Parameters<Action>]): ReturnType<Action> {
   return action(...params);
 }
 
