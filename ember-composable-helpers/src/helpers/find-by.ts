bbfad1d4ccfbac55a33701bfd71893bfdb780c4d
Peter Wagenet <peter@wagenet.us>
2025-01-06 09:50:57 -0800

    Fix a bug

--- ember-composable-helpers/src/helpers/find-by.ts
+++ ember-composable-helpers/src/helpers/find-by.ts
@@ -5,7 +5,7 @@ import asArray from '../utils/as-array.ts';
 
 export function findBy<T>([byPath, value, array]: [keyof T, T[keyof T], T[]]) {
   if (isEmpty(byPath)) {
-    return [];
+    return undefined;
   }
 
   return asArray(array).find((item) => get(item, String(byPath)) === value);
